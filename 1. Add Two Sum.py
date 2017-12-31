6020a3c68142037adbd3c59ce04a908d49ae0d88
Eddy(Hong) Jia <h.jia@student.unsw.edu.au>
2017-12-31 11:17:05 +1100

    Update 1. Add Two Sum.py

--- 1. Add Two Sum.py
+++ 1. Add Two Sum.py
@@ -1,8 +1,12 @@
+
+#enumerate同时扫描索引和值
+
+
 nums = [2, 7, 11, 15]
 target = 9
 hash_map={}
 for index, value in enumerate(nums):
-    hash_map[value] = index #find both index and value's position
+    hash_map[value] = index #同时找出索引和值，注意顺序
     print(hash_map)
 for index1, value in enumerate(nums):
     if target - value in hash_map:
