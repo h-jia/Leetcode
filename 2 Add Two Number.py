3f4b0792f658cfb02545193ee716b26dbc2c9fe0
Eddy <h.jia@student.unsw.edu.au>
2018-01-03 18:50:11 +1100

    Update 2 Add Two Number.py

--- 2 Add Two Number.py
+++ 2 Add Two Number.py
@@ -1,4 +1,5 @@
-#在python中，一边循环一边计算的机制，称为生成器（Generator）——>节省内存，可用while或者def+yield
+#在python中，一边循环一边计算的机制，称为生成器（Generator）——>节省内存，可用while或者def+yield。
+#https://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000/00138681965108490cb4c13182e472f8d87830f13be6e88000
 
 
 
